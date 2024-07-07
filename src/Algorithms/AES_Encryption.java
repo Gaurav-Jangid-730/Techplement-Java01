c06fd97b7df03fc4680f7ba87b0413315ce86e9a
Sunil Choudhary <140395985+SmartHacker8@users.noreply.github.com>
2024-07-07 10:46:31 +0530

    Update AES_Encryption.java

--- src/Algorithms/AES_Encryption.java
+++ src/Algorithms/AES_Encryption.java
@@ -1,4 +1,41 @@
 package Algorithms;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.SecureRandom;
 
 public class AES_Encryption {
+
+    public static final String ALGORITHM = "AES";
+
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+        keyGen.init(256, new SecureRandom());
+        return keyGen.generateKey();
+    }
+
+    public static byte[] encrypt(byte[] data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] decrypt(byte[] data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    public static void saveKey(String key, String fileName) throws IOException {
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            fileWriter.write(key);
+        }
+    }
+
+    public static String loadKey(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
 }
